5df33d0bc8f3fc247dd1d6d95d02657163d87c2e
jorgesilva <jorge.lima2407@gmail.com>
2023-05-06 10:59:50 -0300

    Adicao de flag a um dos comandos do script

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ for file in input/*
 do  
     bin/main.exe -c input/$(basename "$file") -o result/$(basename "$file").z78
     bin/main.exe -x result/$(basename "$file").z78 -o result/$(basename "$file")
-    la input/$(basename "$file") result/$(basename "$file").z78
+    ls input/$(basename "$file") result/$(basename "$file").z78 -lh
     diff input/$(basename "$file") result/$(basename "$file") -q
 done
-la input/ result/*.z78
+ls input/ result/*.z78 -lh
